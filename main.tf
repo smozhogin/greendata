53c9eeeaa516babf53f908fec3af41411cbd4d40
Sergey Mozhogin <s_mozhogin@yahoo.com>
2025-12-22 22:00:24 +0300

    CI/CD Deploy Test

--- main.tf
+++ main.tf
@@ -10,6 +10,10 @@ variable "api_image_url" {
     type = string
 }
 
+variable "ui_image_url"  {
+    type = string
+}
+
 terraform {
     required_version = ">= 1.3.0"
 
@@ -29,10 +33,9 @@ resource "yandex_serverless_container" "greendata_api" {
     name               = "greendata-api"
     folder_id          = var.folder_id
     service_account_id = var.service_account_id
-
-    cores       = 4
-    memory      = 4096
-    concurrency = 1
+    cores              = 4
+    memory             = 4096
+    concurrency        = 1
 
     provision_policy {
         min_instances = 1
@@ -43,6 +46,27 @@ resource "yandex_serverless_container" "greendata_api" {
     }
 }
 
+resource "yandex_serverless_container" "greendata_ui" {
+    name               = "greendata-ui"
+    folder_id          = var.folder_id
+    service_account_id = var.service_account_id
+    cores              = 2
+    memory             = 2048
+    concurrency        = 1
+
+    provision_policy {
+        min_instances = 1
+    }
+
+    image {
+        url = var.ui_image_url
+    }
+}
+
 output "api_url" {
     value = yandex_serverless_container.greendata_api.url
+}
+
+output "ui_url" {
+    value = yandex_serverless_container.greendata_ui.url
 }
